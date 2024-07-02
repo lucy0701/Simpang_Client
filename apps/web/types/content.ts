afaa2e2e7beecfb396ce2c96dd97748b3c2ca37c
Crystal P <sj8871@gmail.com>
2024-07-02 23:26:49 +0900

    feat: add home page

--- apps/web/types/content.ts
+++ apps/web/types/content.ts
@@ -1,5 +1,23 @@
 export type ContentType = 'MBTI';
 
+export interface Contents {
+  _id: string;
+  title: string;
+  content: string;
+  imageUrl: string;
+  playCount: number;
+  commentCount: number;
+  likeCount: number;
+  type: ContentType;
+}
+
+export interface ContentsData {
+  totalCount: number;
+  totalPage: number;
+  currentPage: number;
+  contents: Contents[];
+}
+
 export interface Question {
   index: number;
   question: string;
--- apps/web/types/index.ts
+++ apps/web/types/index.ts
@@ -1,1 +1,9 @@
 export * from './login';
+export * from './content';
+export * from './result';
+
+export interface PageParams {
+  page: number;
+  size: number;
+  sort: number;
+}
