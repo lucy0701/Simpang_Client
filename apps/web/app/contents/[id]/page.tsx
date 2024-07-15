2349c82cf0dad8c25e393441abdfcfa0e8ff60fc
Crystal P <sj8871@gmail.com>
2024-07-16 00:32:07 +0900

    fix:  Add cache: 'no-store' to the content page data fetching function. - 캐싱된 데이터로, 데이터 호출 시 차이가 발생 하여 추가함

--- apps/web/app/contents/[id]/page.tsx
+++ apps/web/app/contents/[id]/page.tsx
@@ -3,9 +3,15 @@ import { IContent } from '@/types';
 
 import ContentContainer from '@/containers/ContentContainers';
 
+async function getData(id: string): Promise<IContent> {
+  const res = await fetch(`${BE_URL}/api/v1/contents/${id}`, { cache: 'no-store' });
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  return res.json();
+}
+
 export default async function Page({ params }: { params: { id: string } }) {
-  const contentData = await fetch(`${BE_URL}/api/v1/contents/${params.id}`).then(
-    (res) => res.json() as Promise<IContent>,
-  );
+  const contentData = await getData(params.id);
   return <ContentContainer contentData={contentData} />;
 }
--- apps/web/app/contents/random/page.tsx
+++ apps/web/app/contents/random/page.tsx
@@ -3,9 +3,15 @@ import { IContent } from '@/types';
 
 import ContentContainer from '@/containers/ContentContainers';
 
+async function getData(): Promise<IContent[]> {
+  const res = await fetch(`${BE_URL}/api/v1/contents/random?size=1`, { cache: 'no-store' });
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  return res.json();
+}
+
 export default async function Page() {
-  const contentData = await fetch(`${BE_URL}/api/v1/contents/random?size=1`).then(
-    (res) => res.json() as Promise<IContent[]>,
-  );
-  return <ContentContainer contentData={contentData[0]!} />;
+  const randomData = await getData();
+  return <ContentContainer contentData={randomData[0]!} />;
 }
