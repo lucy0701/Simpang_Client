b9830dd41c69a072bc237eb7e3101ca6815d6951
Crystal P <sj8871@gmail.com>
2024-07-25 21:33:19 +0900

    fix: delete random page - 랜덤 버튼 : 버튼에서 random API 함수 실행. content/:id page로 이동하도록 변경

--- apps/web/components/ButtonBox/FloatBtnBox/index.tsx
+++ apps/web/components/ButtonBox/FloatBtnBox/index.tsx
@@ -1,8 +1,10 @@
+import { useQuery } from '@tanstack/react-query';
 import cx from 'classnames';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 import { PATHS } from '@/constants';
+import { getRandomContentAPI } from '@/services/contents';
 import { FloatButtonPosition } from '@/types';
 
 import styles from './index.module.scss';
@@ -27,14 +29,24 @@ export const FloatTopBtn = ({ position = 'left' }: Props) => {
 };
 
 export const RandomButton = ({ position = 'right' }: Props) => {
+  const { data } = useQuery({
+    queryKey: ['randomContent'],
+    queryFn: () => getRandomContentAPI({ size: 1 }),
+    staleTime: 0,
+    gcTime: 0,
+  });
+
   const router = useRouter();
+
+  const onClickRandomBtn = () => router.push(PATHS.CONTENTS.BASE + `/${data![0]!._id}`);
+
   return (
     <Button
       text="Random"
       skin="float"
       color="green"
       position={position}
-      onClick={() => router.push(PATHS.CONTENTS.RAMDOM)}
+      onClick={onClickRandomBtn}
     />
   );
 };
