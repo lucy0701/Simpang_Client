fdaf652a9c60cace1608c849f37c2e021d26f172
Crystal P <sj8871@gmail.com>
2024-07-31 20:52:50 +0900

    refactor: gtm-script.js 파일 분리 , mata - verification 정리

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -15,6 +15,10 @@ export const metadata: Metadata = {
   metadataBase: url,
   title,
   description,
+  verification: {
+    google: 'HFApnmNt0MJobDHpjw9go06M0GkuMMZFG4fXvljTkQs',
+    other: { 'naver-site-verification': 'd247534d9de576f74edc6b0b3eabf55b3350835c' },
+  },
   icons: {
     icon: '/favicon.ico',
   },
@@ -60,25 +64,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script strategy="afterInteractive" id="gtm-script">
-          {`
-            (function(w, d, s, l, i) {
-              w[l] = w[l] || [];
-              w[l].push({'gtm.start':
-                new Date().getTime(), event: 'gtm.js'});
-              var f = d.getElementsByTagName(s)[0],
-                  j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-              j.async = true;
-              j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-              f.parentNode.insertBefore(j, f);
-            })(window, document, 'script', 'dataLayer', 'GTM-WCT3PX2H');
-          `}
-        </Script>
-        <meta
-          name="google-site-verification"
-          content="HFApnmNt0MJobDHpjw9go06M0GkuMMZFG4fXvljTkQs"
-        />
-        <meta name="naver-site-verification" content="d247534d9de576f74edc6b0b3eabf55b3350835c" />
+        <Script src="/scripts/gtm-script.js" strategy="afterInteractive" />
         <script
           async
           src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.2/kakao.js"
