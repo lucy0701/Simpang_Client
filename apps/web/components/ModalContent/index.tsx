89f23cfce26f3ba162e60010e5fa4534a7f17f7f
Crystal P <sj8871@gmail.com>
2024-07-31 16:28:30 +0900

    feat: 모달 외부 클릭시 팝업 닫힘, 모달 배경 스크롤바 막기 - modalRef 사용하여 외부 클릭 닫기 구현 - 모달 배경 스크롤 막기   - base style의 'mobileCoverOpen' class name을 활용 (side nav bar에서 동일하게 사용)

--- apps/web/components/ModalContent/index.tsx
+++ apps/web/components/ModalContent/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 
 import styles from './index.module.scss';
 import WindowStyle from '../WindowStyles';
@@ -22,6 +22,8 @@ const ModalContent = ({
   onClickCheckBtn,
   onClickCancelBtn,
 }: Props) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+
   const handleKeyDown = useCallback(
     (event: KeyboardEvent) => {
       if (event.key === 'Escape') {
@@ -34,23 +36,38 @@ const ModalContent = ({
     [onClickCancelBtn],
   );
 
+  const handleClickModalOutside = useCallback(
+    (event: MouseEvent) => {
+      if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+        onClickCancelBtn();
+      }
+    },
+    [onClickCancelBtn],
+  );
+
   useEffect(() => {
     if (showModal) {
       document.addEventListener('keydown', handleKeyDown);
+      document.addEventListener('mousedown', handleClickModalOutside);
+      document.documentElement.classList.add('mobileCoverOpen');
     } else {
       document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('mousedown', handleClickModalOutside);
+      document.documentElement.classList.remove('mobileCoverOpen');
     }
 
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('mousedown', handleClickModalOutside);
     };
   }, [showModal, handleKeyDown]);
 
   if (!showModal) return null;
+
   return (
     showModal && (
       <div className={styles.wrap}>
-        <div className={styles.modalContainer}>
+        <div className={styles.modalContainer} ref={modalRef}>
           <div className={styles.modalWrap}>
             <button className={styles.topCloseBtn} onClick={onClickCancelBtn} />
             <WindowStyle title={title} color="blue">
