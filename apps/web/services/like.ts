715e671733f93173446df94499eede103f907871
Crystal P <sj8871@gmail.com>
2024-08-08 01:56:37 +0900

    feat: like type, api, image 추가

--- apps/web/services/like.ts
+++ apps/web/services/like.ts
@@ -1,4 +1,8 @@
+import { GetPageData, PageParams } from '@/types';
+
 import { apiBe, getHeaders } from '.';
+
+import { ILikeContent } from '@/types/like';
 
 interface LikeData {
   liked: boolean;
@@ -13,5 +17,11 @@ export const getLikeAPI = (contentId: string) => {
 export const postLikeAPI = async (contentId: string) => {
   const headers = getHeaders();
   const res = await apiBe.post<LikeData>(`/v1/likes/${contentId}`, {}, { headers });
+  return res.data;
+};
+
+export const getLikeContentsAPI = async (params: PageParams) => {
+  const headers = getHeaders();
+  const res = await apiBe<GetPageData<ILikeContent>>(`/v1/likes`, { params, headers });
   return res.data;
 };
